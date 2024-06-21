29048e91bf775875fc4779586c729620b0d9d51b
Berk Özden <berkozden5@gmail.com>
2024-06-22 00:56:28 +0300

    Catalog microservice's category api addresses have been centralised.

---
+++ Frontend/MultiShop.WebUI/Services/CatalogServices/CategoryService/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Dtos.CatalogDtos.CategoryDtos;
+
+namespace MultiShop.WebUI.Services.CatalogServices.CategoryService
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoriesAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id);
+    }
+}
